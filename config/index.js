47a9efdd8d271be78f5b8284e0ca5636428c3687
manoj <manojgodhani00@gmail.com>
2019-07-20 18:42:45 +0530

    dd

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ module.exports = {
     LOOPBACK_CLIENT_ID: 'LOOPBACK_CLIENT_ID',
     TURN_BASE_URL: 'https://live-ttalk.herokuapp.com',
     TURN_URL_TEMPLATE: '%s/turn',
-    WSS_HOST_PORT_PAIRS: ['live-ttalk-ws.herokuapp.com'],
+    WSS_HOST_PORT_PAIRS: ['node-apprtc-ws.herokuapp.com'],
     RESPONSE_UNKNOWN_ROOM: 'UNKNOWN_ROOM',
     RESPONSE_UNKNOWN_CLIENT: 'UNKNOWN_CLIENT',
     RESPONSE_ROOM_FULL: 'FULL',
